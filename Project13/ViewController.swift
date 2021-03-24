bedb7eac41f5b41994c59feb860c1db021f8e22e
FrankSolleveld <fsolleveld@outlook.com>
2021-03-24 11:06:36 +0100

    added base methods

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: -- Outlets & Variables
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    // MARK: -- Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: -- Custom Methods
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    // MARK: -- (Delegate) Methods
 
 
 }
