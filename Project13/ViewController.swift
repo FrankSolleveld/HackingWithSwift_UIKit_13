2451aafe7bb7110f9d30c96a579ddefbbcb05a80
FrankSolleveld <fsolleveld@outlook.com>
2021-03-24 11:19:21 +0100

    added delegate

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -7,16 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: -- Outlets & Variables
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     
+    var currentImage: UIImage!
+    
     // MARK: -- Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
     
     // MARK: -- Custom Methods
@@ -29,8 +33,17 @@ class ViewController: UIViewController {
     @IBAction func intensityChanged(_ sender: Any) {
     }
     
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
     // MARK: -- (Delegate) Methods
-
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true, completion: nil)
+        currentImage = image
+    }
 }
-
